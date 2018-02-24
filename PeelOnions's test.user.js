081985f5d10c41b307dff6519e641baf1ce040ef
杜俊 <33235405+jjandnn@users.noreply.github.com>
2018-02-25 05:44:23 +0800

    Add files via upload
    测试油猴js脚本，给个地址

---
+++ PeelOnions's test.user.js
@@ -0,0 +1,22 @@
+// PeelOnions's alert
+// version 0.1 BETA!
+// 2018-02-25
+// Copyright (c) 2018, PeelOnions
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+// -------------------------------------------------------------
+// ==UserScript==
+// @name PeelOnions's alert
+// @namespace http://peelonions.net
+// @description 在用户的浏览页上弹出提醒信息，加强对方的互联网安全意识！
+// -------------------------------------------------------------
+// @include *
+// @exclude http://diveintogreasemonkey.org/*
+// @exclude http://www.diveintogreasemonkey.org/*
+// @exclude http://peelonions.net/*
+// @exclude http://www.peelonions.net/*
+// @exclude https://peelonions.net/*
+// @exclude https://www.peelonions.net/*
+// ==/UserScript==
+// -------------------------------------------------------------
+alert('二货，谁让你这么容易轻信别人，随便点击乱七八糟的东西，呵呵！  你的支付宝账号密码我拿走了!——顶级黑客组织：PeelOnions');
